ca2368fbc9d7117f5d8459982f2c6b809b25a452
davideorsini <dav_094@hotmail.it>
2018-02-17 17:21:15 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,14 @@
 import socket
-import datetime
-
 from flask import Flask
+import calendar
+import time
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    flog = open('/mnt/log.txt', 'a')
-#     text = "" + socket.gethostname() + time.time() + "\n"
-#     flog.write(text)
-    flog.close()
-    return "Hello World! Greetings from "+ socket.gethostname() + datetime.datetime.utcnow() + "\n"
+ 
+    return "Hello World! Greetings from "+ socket.gethostname() + calendar.timegm(time.gmtime()) + "\n"
 
 
 if __name__ == "__main__":
