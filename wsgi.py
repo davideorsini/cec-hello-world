559a6c6479a6edf9429eec93449ef3f54a62cc82
davideorsini <dav_094@hotmail.it>
2018-02-17 16:33:37 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,10 +6,10 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    #flog = open('log.txt', 'a')
+    flog = open('log.txt', 'a')
     #flog.write(socket.gethostname() + time.time())
-    #flog.close()
-    return "Hello World! Greetings from "+socket.gethostname()+"zio santuzzo\n"
+    flog.close()
+    return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
 
 if __name__ == "__main__":
