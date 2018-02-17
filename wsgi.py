df71dc4d74b250028855d48ecef2cba1ab39460d
davideorsini <dav_094@hotmail.it>
2018-02-17 18:02:38 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,25 +3,17 @@ from flask import Flask
 from datetime import datetime
 
 application = Flask(__name__)
-
+    
 @application.route("/")
-def write_logfile():
+def hello():
     #create logfile in persistent storage with write append permission and write timestamp and pod
     with open("/mnt/logfile", "a") as file:
         file.write(str(datetime.now()) +": " + socket.gethostname() + "<br>\n") 
-
-@application.route("/")
-def get_from_logfile():
+    
     #open logfile and return its contents
     fr = open("/mnt/logfile","r")
     log = fr.read()
     fr.close()
-    return txt
-    
-@application.route("/")
-def hello():
-    write_logfile()
-    log = get_from_logfile()
     text = "<html><title> Logfile <br></title><body>" + log + "</body></html>"
     return text
 
