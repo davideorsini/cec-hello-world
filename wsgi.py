417689c84e9c1d06f3cb68c9f9cf5dbee4d797fb
davideorsini <dav_094@hotmail.it>
2018-02-17 16:37:08 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     flog = open('/mnt/log.txt', 'a')
-    flog.write(socket.gethostname() + time.time() + "\n")
+    flog.write("ciao")
     flog.close()
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
